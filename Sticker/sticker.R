154290b0df9a1b052284bd5ba928c87d8abac0d6
Rakesh Poduval <poduval.rakesh@icloud.com>
2023-09-02 16:05:53 +0530

    produced sticker

--- Sticker/sticker.R
+++ Sticker/sticker.R
@@ -2,14 +2,14 @@
 library(hexSticker)
 
 sticker_icon <- sticker(
-  "pictures/tax_calculator.png", 
+  "sticker/indian_coin_back.png", 
   s_x = 1, s_y = .75, 
   s_width = 0.4, s_height = 0.4,
-  p_size = 20, p_color = colorDarkBrown, p_family = "Aller_Rg", p_fontface = "plain",
+  p_size = 15, p_color = "brown", p_family = "Aller_Rg", p_fontface = "plain",
   h_size = 1.2, h_fill = "lightgrey", h_color = "#D78113",
   spotlight = FALSE,
   dpi = 300,
-  package = "taxcalculator", 
-  filename = "stickers/tax_calculator_logo.png")
+  package = "Easy Calculator", 
+  filename = "Sticker/logo.png")
 
 sticker_icon
